828f81c03464325c8bba34b0b5f86914502cf065
julian-morales-unad <julianttm@gmail.com>
2017-04-15 12:53:37 -0500

    Más Contenido

---
+++ js/colores.js
@@ -0,0 +1,26 @@
+var colorRojo = document.querySelector("#colorRojo");
+var colorVerde = document.querySelector("#colorVerde");
+var colorAzul = document.querySelector("#colorAzul");
+
+var catidadRojo = document.querySelector("#catidadRojo");
+var catidadVerde = document.querySelector("#catidadVerde");
+var catidadAzul = document.querySelector("#catidadAzul");
+
+var cuadroColores = document.querySelector("#cuadroColores");
+
+colorRojo.addEventListener("input", function () {
+    cantidadRojo.innerHTML = this.value;
+    cambiarColor();
+});
+colorVerde.addEventListener("input", function () {
+    cantidadVerde.innerHTML = this.value;
+    cambiarColor();
+});
+colorAzul.addEventListener("input", function () {
+    cantidadAzul.innerHTML = this.value;
+    cambiarColor();
+});
+
+function cambiarColor(){
+    cuadroColores.style.background = "rgb(" + colorRojo.value + "," + colorVerde.value + "," + colorAzul.value + ")" ;
+}
